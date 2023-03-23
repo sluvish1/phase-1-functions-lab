920c36ea5ed7d213dd4bf13e4484069ab2733b19
Vaschell Mkenzie <mvaschell@gmail.com>
2023-03-23 17:25:21 -0400

    travels

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+const headQuaters = 42;
+function distanceFromHqInBlocks(blocks){  
+   return Math.abs(blocks-headQuaters)
+}
+
+function distanceFromHqInFeet(blocks){
+   distanceFromHqInBlocks(blocks)
+   return Math.abs(distanceFromHqInBlocks(blocks)*264)
+}
+
+
+function distanceTravelledInFeet(distance,blocks){
+    distanceFromHqInFeet(blocks)
+return Math.abs ((blocks-distance)*264)
+}
+
+
+function calculatesFarePrice(blocks, distance){
+let feetTraveled = distanceTravelledInFeet(distance, blocks)
+   if(feetTraveled<=400){
+    return 0
+   }
+   else if(feetTraveled>400 && feetTraveled<2000){
+    return 2.56
+   }
+   else if(feetTraveled>2000 && feetTraveled<2500 ){
+    return 25
+   }
+   else{
+    return 'cannot travel that far'
+   }
+
+}
